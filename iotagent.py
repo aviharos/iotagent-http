71d4f62db008119d103f1aa1967398fd8c0e51d3
panda-mechanic <m2vihand@gmail.com>
2022-05-23 22:31:06 +0200

    HTTP PUT working with JSON payload

--- iotagent.py
+++ iotagent.py
@@ -72,7 +72,7 @@ class Server(BaseHTTPRequestHandler):
     def decode_request(self, post_data):
         all_data = json.loads(post_data)
         if type(all_data) is not dict:
-            raise ValueError(f'Error.\nThe sent raw data does not contain a json:\n{post_data}')
+            raise ValueError(f'The sent raw data does not contain a json:\n{post_data}')
 
         for key in all_data.keys():
             if key != key.lower().strip():
@@ -80,11 +80,11 @@ class Server(BaseHTTPRequestHandler):
                 del all_data[key]
 
         if 'method' not in all_data.keys():
-            raise KeyError(f'Error.\nThe decoded json:{all_data} does not include the key: \'method\'')
+            raise KeyError(f'The decoded json:{all_data} does not include the key: \'method\'')
         all_data['method'] = all_data['method'].upper().strip()
         if all_data['method'] not in ('GET', 'HEAD', 'POST', 'PUT', 'DELETE',
                                       'CONNECT', 'OPTIONS', 'TRACE'):
-            raise ValueError('Error.\nNot a valid HTTP method:{}'.format(all_data['method']))
+            raise ValueError('Not a valid HTTP method:{}'.format(all_data['method']))
 
         if all_data['method'] not in ('GET', 'POST', 'PUT', 'DELETE'):
             raise NotImplementedError('Not implemented HTTP method: {}'.format(all_data['method']))
@@ -95,20 +95,24 @@ class Server(BaseHTTPRequestHandler):
 
         for key in mandatory_keys:
             if key not in all_data.keys():
-                raise KeyError(f'Error.\nThe decoded json:{all_data} does not include the key: \'{key}\'')
+                raise KeyError(f'The decoded json:{all_data} does not include the key: \'{key}\'')
 
         all_data['url'] = all_data['url'].strip()
         # raise validators.ValidationError if not valid url
         validators.url(all_data['url'])
 
+        logger.debug('Unprocessed headers:\n{}\nType: {}'.format(all_data['headers'], type(all_data['headers'])))
+
         try:
             headers = {}
-            for header in all_data[headers]:
+            for header in all_data['headers']:
                 header = str(header)
                 name, value = header.split(':')[0].strip(), header.split(':')[1].strip()
                 headers[name] = value
         except IndexError:
             raise IndexError(f'The decoded header: {header} does not have a structure of\nkey: value')
+
+        logger.debug('Processed headers:\n{}'.format(headers))
 
         if all_data['method'] in ('GET', 'DELETE'):
             req = HTTPRequest(url=all_data['url'],
@@ -116,44 +120,57 @@ class Server(BaseHTTPRequestHandler):
                               headers=headers)
             return req
 
-        try:
-            data_json = json.loads(all_data['data'])
-        except json.JSONDecodeError:
-            data_raw = all_data['data']
-            # now the request must be either POST or PUT, require a body
-            if len(data_raw) == 0:
-                raise ValueError('Error.\nThe decoded request has a method {}, but has no data.'.format(all_data['method']))
+        if 'Content-Type' not in headers.keys():
+            raise ValueError('Missing header: "Content-Type: application/json" or "Content-Type: text/plain"')
 
-            req = HTTPRequest(url=all_data['url'],
-                              method=all_data['method'],
-                              headers=headers,
-                              data_type='raw',
-                              data_raw=data_raw)
-        else:
+        if headers['Content-Type'] not in ('application/json', 'text/plain'):
+            raise ValueError('Unsupported Content-Type: {}\nSupported Content-Types: "application/json", "text/plain"')
+
+        if headers['Content-Type'] == 'application/json':
+            if type(all_data['data']) is not dict:
+                raise ValueError('Content-Type is application/json, but the data does not contain a json: {}'.format(all_data['data']))
+            data_str = str(all_data['data']).replace('\'', '"')
+            headers['Content-Length'] = str(len(data_str))
             req = HTTPRequest(url=all_data['url'],
                               method=all_data['method'],
                               headers=headers,
                               data_type='json',
-                              data_json=data_json)
+                              data_json=data_str)
+
+        if headers['Content-Type'] == 'text/plain':
+            # TODO test
+            # now the request must be either POST or PUT, require a body
+            data_str = all_data['data']
+            headers['Content-Length'] = str(len(data_str))
+            if len(data_str) == 0:
+                raise ValueError('The decoded request has a method {}, but has no data.'.format(all_data['method']))
+            req = HTTPRequest(url=all_data['url'],
+                              method=all_data['method'],
+                              headers=headers,
+                              data_type='raw',
+                              data_raw=all_data['data'])
         return req
 
     def send_request_to_broker(self, req):
         if req.method == 'GET':
+            # TODO test
             res = requests.get(url=req.url, headers=req.headers)
 
         elif req.method == 'POST':
+            # TODO test
             if req.data_type == 'raw':
-                res = requests.post(url=req.url, headers=req.headers, data_raw=req.data_raw)
+                res = requests.post(url=req.url, headers=req.headers, data=req.data_raw)
             elif req.data_type == 'json':
-                res = requests.post(url=req.url, headers=req.headers, data_json=req.data_json)
+                res = requests.post(url=req.url, headers=req.headers, data=req.data_json)
                 
         elif req.method == 'PUT':
             if req.data_type == 'raw':
-                res = requests.put(url=req.url, headers=req.headers, data_raw=req.data_raw)
+                res = requests.put(url=req.url, headers=req.headers, data=req.data_raw)
             elif req.data_type == 'json':
-                res = requests.put(url=req.url, headers=req.headers, data_json=req.data_json)
+                res = requests.put(url=req.url, headers=req.headers, data=req.data_json)
 
         elif req.method == 'DELETE':
+            # TODO test
             res = requests.delete(url=req.url, headers=req.headers)
 
         res.close()
@@ -170,7 +187,6 @@ class Server(BaseHTTPRequestHandler):
         logger.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
                     str(self.path), str(self.headers), post_data.decode('utf-8'))
 
-        self._set_response()
         try:
             request = self.decode_request(post_data)
         except (ValueError or
@@ -179,24 +195,31 @@ class Server(BaseHTTPRequestHandler):
                 NotImplementedError or
                 validators.ValidationError or
                 json.JSONDecodeError) as error:
+            # TODO test
             logger.error(f'Error decoding message. Traceback:\n{error}')
-            self.wfile.write("POST request for {}\nError decoding message.\nTraceback:\n{}".format((self.path).encode('utf-8'), error))
+            self.send_response(400)
+            self.end_headers()
+            self.wfile.write("POST request for {}\nError decoding message.\nTraceback:\n{}".format((self.path).encode('utf-8'), str(error).encode('utf-8')))
         else:
             logger.debug(f'Request decoded:\n{request}')
             res = self.send_request_to_broker(request)
-            self.wfile.write("POST request for {}\nOrion response:{}".format((self.path).encode('utf-8'), res))
+            logger.info(f'Orion response:\n{res}')
+            self.send_response(res.status_code)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("POST request for {}\nOrion status code: {}".format((self.path).encode('utf-8'), str(res.status_code)).encode('utf-8'))
 
 
 def run(server_class=HTTPServer, handler_class=Server, port=8070):
     server_address = ('', port)
     http_service = server_class(server_address, handler_class)
-    logger.info(f'Starting http server on port {port}...')
+    logger.info(f'Starting PLC IoT agent server on port {port}...')
     try:
         http_service.serve_forever()
     except KeyboardInterrupt:
         pass
     http_service.server_close()
-    logger.info('KeyboardInterrupt. Stopping http server...')
+    logger.info('KeyboardInterrupt. Stopping PLC IoT agent...')
 
 
 if __name__ == '__main__':
