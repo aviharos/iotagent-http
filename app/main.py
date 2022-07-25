0021426b630d68e9f5cef518c820499c2067ed77
panda-mechanic <m2vihand@gmail.com>
2022-07-25 22:17:49 +0200

    dinc -> $dinc

--- app/main.py
+++ app/main.py
@@ -169,9 +169,9 @@ class IoTAgent(BaseHTTPRequestHandler):
             return req
         elif parsed_data['method'] in ('POST', 'PUT'):
             if headers['Content-Type'] == 'application/json':
-                data = str(parsed_data['data']).replace('\'', '"').replace('dinc', '$inc')
+                data = str(parsed_data['data']).replace('\'', '"').replace('"dinc"', '"$inc"')
             elif headers['Content-Type'] == 'text/plain':
-                data = parsed_data['data'].replace('dinc', '$inc')
+                data = parsed_data['data'].replace('"dinc"', '"$inc"')
             headers['Content-Length'] = str(len(data))
             req = HTTPRequest(url=parsed_data['url'],
                               method=parsed_data['method'],
