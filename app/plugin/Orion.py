cb1f581e60fd110e664b7ad418bef67e0e656a56
aviharos <vihand@protonmail.ch>
2022-09-22 14:28:03 +0200

    feat: start working on plugin support

---
+++ app/plugin/Orion.py
@@ -0,0 +1,158 @@
+﻿# -*- coding: utf-8 -*-
+"""
+A set of modules for interacting with the Orion broker
+
+The Orion host and port are read from the environment variables
+
+Environment variables:
+    ORION_HOST: the URL of the Orion broker
+    ORION_PORT: the port of the Orion broker
+
+Raises:
+    RuntimeError: if the Orion_HOST is not set
+"""
+# Standard Library imports
+import os
+
+# PyPI packages
+import requests
+
+# Custom imports
+# from modules.log_it import log_it
+from Logger import getLogger
+
+logger_Orion = getLogger(__name__)
+
+# environment variables
+ORION_HOST = os.environ.get("ORION_HOST")
+if ORION_HOST is None:
+    raise RuntimeError("Critical: ORION_HOST environment variable is not set")
+
+ORION_PORT = os.environ.get("ORION_PORT")
+if ORION_PORT is None:
+    default_port = 1026
+    logger_Orion.warning(
+        f"ORION_PORT environment variable not set, using default value: {default_port}"
+    )
+    ORION_PORT = default_port
+
+
+def getRequest(url: str):
+    """Send a GET request to Orion
+
+    Args:
+        url (str): any Orion that is suitable for GET requests
+
+    Returns:
+        the response status code and the json
+
+    Raises:
+        RuntimeError: when the request fails for any reason
+        ValueError: if the json parsing fails
+    """
+    try:
+        response = requests.get(url)
+        response.close()
+    except Exception as error:
+        raise RuntimeError(f"Get request failed to URL: {url}") from error
+
+    else:
+        try:
+            response.json()
+        except requests.exceptions.JSONDecodeError as error:
+            raise ValueError(
+                f"The JSON could not be decoded after GET request to {url}. Response:\n{response}"
+            ) from error
+        return response.status_code, response.json()
+
+
+def get(object_id: str, host: str=ORION_HOST, port: int =ORION_PORT):
+    """Get an object from Orion identified by the ID
+
+    Args:
+        object_id (str): the Orion object id
+        host (str): Orion host. Default: ORION_HOST environment variable
+        port (int): Orion port. Default: ORION_PORT environment variable
+
+    Returns:
+        The object in JSON format idenfitied by object_id
+
+    Raises:
+        RuntimeError: if the get request's status code is not 200
+    """
+    url = f"http://{host}:{port}/v2/entities/{object_id}"
+    logger_Orion.debug(url)
+    status_code, json_ = getRequest(url)
+    if status_code != 200:
+        raise RuntimeError(
+            f"Failed to get object from Orion broker:{object_id}, status_code:{status_code}; no OEE data"
+        )
+    return json_
+
+
+def exists(object_id: str):
+    """Check if an object exists in Orion
+
+    Args:
+        object_id (str): the object's id in Orion
+
+    Returns:
+        True if the object idenfitied by object_id exists,
+        False otherwise.
+    """
+    try:
+        get(object_id)
+        return True
+    except RuntimeError:
+        return False
+
+
+def getWorkstations():
+    """Download all Workstation objects at once from Orion
+
+    Returns:
+        A list of the Workstation objects
+
+    Raises:
+        RuntimeError: if the get request's status_code is not 200
+    """
+    url = f"http://{ORION_HOST}:{ORION_PORT}/v2/entities?type=Workstation"
+    status_code, workstations = getRequest(url)
+    if status_code != 200:
+        raise RuntimeError(
+            f"Critical: could not get Workstations from Orion with GET request to URL: {url}"
+        )
+    return workstations
+
+
+def update(objects: list):
+    """Updates the objects in Orion
+
+    This method takes an iterable (objects) that contain Orion objects
+    then updates them in Orion.
+    If an object already exists, it will be overwritten. More information:
+    https://github.com/FIWARE/tutorials.CRUD-Operations#six-request
+
+    Args:
+        objects: an iterable containing Orion objects
+
+    Raises:
+        TypeError: if the objects does not contain an iterable
+        RuntimeError: if the POST request's status code is not 204
+    """
+    logger_Orion.debug(f"update: objects: {objects}")
+    url = f"http://{ORION_HOST}:{ORION_PORT}/v2/op/update"
+    try:
+        json_ = {"actionType": "append", "entities": list(objects)}
+        logger_Orion.debug(f"update: json_: {json_}")
+    except TypeError as error:
+        raise TypeError(
+            f"The objects {objects} are not iterable, cannot make a list. Please, provide an iterable object"
+        ) from error
+    response = requests.post(url, json=json_)
+    if response.status_code != 204:
+        raise RuntimeError(
+            f"Failed to update objects in Orion.\nStatus_code: {response.status_code}\nObjects:\n{objects}"
+        )
+    else:
+        return response.status_code
---
+++ app/plugin/transform.py
@@ -0,0 +1,71 @@
+"""The main part of the plugin that transforms the HTTP request 
+
+The transform function will be applied to the HTTPRequest object before sending it. 
+"""
+import sys
+import Orion
+
+sys.path.insert(0, "..")
+from HTTPRequest import HTTPRequest
+
+def transform(req: HTTPRequest):
+    """Transform a HTTPRequest object 
+
+    Args:
+        req (HTTPRequest): request to transform 
+
+    Returns:
+        a transformed HTTPRequest object
+    
+    In this Robo4Toys TTE's use case, the transform function will help
+    updating the counters of the Jobs. 
+    The problem is that the PLC does not know the current Job to set,
+    so it cannot provide all necessary information
+    The PLC will send data packets as follows:
+
+        {
+            "url": "http://orion:1026/v2/entities",
+            "method": "PUT",
+            "headers": [],
+            "data": null,
+            "transform": {
+                "ws": "urn:ngsi_ld:Workstation:1",
+                "ct": "good", # counter type
+                "cv": 14, # counter value
+            }
+        }
+
+    This means that the urn:ngsi_ld:Workstation:1 signals that good parts were created,
+    the number of successful cycles so far is 14. 
+    The transform module needs to transform this as follows. 
+    Find the Workstation's Job. Find the Job's part. Find the PartsPerCycle. 
+    Multiply the counter by the PartsPerCycle value to get the actual value of the Job's counter. 
+    Set the counter accordingly: the GoodPartCounter if "ct" contains "good", the RejectPartCounter otherwise.
+
+   For example if the current Job is Job:202200045, the HTTPRequest will be transformed to the equivalent of
+
+   curl --location --request PUT 'http://orion:1026/v2/entities/urn:ngsi_ld:Job:202200045/attrs/GoodPartCounter/value' \
+--header 'Content-Type: text/plain' \
+--data-raw '96'
+
+    PartsPerCycle: 8
+    So the GoodPartCounter's real value must be 8*14 = 96
+
+    Steps:
+        1. Check if the transform attribute of the HTTPRequest is empy or not
+            If empty, the transform function returns the request unchanged
+        2. If the transform attribute of the HTTPRequest does not contain a valid Orion id,
+            the transform function returns the request unchanged
+        3. Get the Workstation Orion object whose id is in the transform attribute
+        4. Get the RefJob attribute
+        5. Get the Job from Orion
+        6. Get the Job's RefPart
+        7. Get the Part from Orion
+        8. Get the Operation based on the CurrentOperationType of the Job 
+        9. Get PartsPerCycle 
+        10. Multiply "cv" by PartsPerCycle to get the Counter
+        11. Check if the GoodPartCounter or the RejectPartCounter needs to be updated (transform["ct"])
+        12. Construct a HTTPRequest that updates the Job's counter.
+        13. Return request
+        """
+    pass
