64223738fd08e75109aebe2f4723991a4263949e
aviharos <vihand@protonmail.ch>
2022-10-05 17:31:49 +0200

    test: add test requests

---
+++ test/test_requests/create_storage.sh
@@ -0,0 +1,12 @@
+curl --location --request POST 'localhost:4315' \
+--header 'Content-Type: text/plain' \
+--data-raw '{"url": "http://orion:1026/v2/entities",
+"method": "POST",
+"headers": ["Content-Type: application/json"],
+"data": {
+"type": "Storage",
+"id": "urn:ngsi_ld:Storage:1",
+"Capacity": {"type": "Number", "value": 100},
+"Counter": {"type": "Number", "value": 100}
+}}
+'
---
+++ test/test_requests/get_storage.sh
@@ -0,0 +1,7 @@
+curl --location --request POST 'localhost:4315' \
+--header 'Content-Type: text/plain' \
+--data-raw '{"url": "http://orion:1026/v2/entities/urn:ngsi_ld:Storage:1",
+"method": "GET",
+"headers": []
+}
+'
